604eca7f89762c7981a5546066aaee200303861f
Mozartted <mozart.osita@gmail.com>
2016-10-31 11:23:36 +0100

    Moved the task route into a group namespaced task

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -15,70 +15,71 @@ use Illuminate\Http\Request;
 use App\Like;
 use Illuminate\Database\Eloquent\Collection;
 
+Route::group(['namespace'=>'tasks'],function(){
+    Route::get('/{task_id?}',function($task_id){
+        $task = Task::find($task_id);
+
+        return Response::json($task);
+    });
+
+    Route::post('/',function(Request $request){
+        $task = Task::create($request->all());
+        $data=[
+            'id'=>$task->id,
+            'task'=>$task->task,
+            'description'=>$task->description,
+            'deadline'=>$task->deadline,
+            'created_at'=>$task->created_at->diffForHumans(),
+
+
+        ];
+
+        return Response::json($data);
+    });
+
+    //adding new like to a task
+    Route::post('/like/{task}',function(Request $request,Task $task){
+        Like::create($request->all());
+
+        $tasklikes=count((array)$task->likes());
+        $likes=[
+            'likes_number'=>$tasklikes,
+        ];
+
+        return Response::json($likes);
+
+    });
+
+    Route::put('/{task_id}',function(Request $request,$task_id){
+        $task = Task::find($task_id);
+
+        $task->task = $request->task;
+        $task->description = $request->description;
+        $task->deadline=$request->deadline;
+
+        $task->save();
+
+        $data=[
+            'id'=>$task->id,
+            'task'=>$task->task,
+            'description'=>$task->description,
+            'created_at'=>$task->created_at->diffForHumans(),
+            'deadline'=>$task->deadline->diffForHumans(),
+        ];
+
+        return Response::json($data);
+    });
+
+    Route::delete('{task_id}',function($task_id){
+        $task = Task::destroy($task_id);
+
+        return Response::json($task);
+    });
+});
 
 Route::get('/', function () {
 
     $tasks = Task::all();
 
     return View::make('welcome')->with('tasks',$tasks);
-});
-
-Route::get('/tasks/{task_id?}',function($task_id){
-    $task = Task::find($task_id);
-
-    return Response::json($task);
-});
-
-Route::post('/tasks',function(Request $request){
-    $task = Task::create($request->all());
-    $data=[
-        'id'=>$task->id,
-        'task'=>$task->task,
-        'description'=>$task->description,
-        'deadline'=>$task->deadline,
-        'created_at'=>$task->created_at->diffForHumans(),
-
-
-    ];
-
-    return Response::json($data);
-});
-
-//adding new like to a task
-Route::post('/tasks/like/{task}',function(Request $request,Task $task){
-    Like::create($request->all());
-
-    $tasklikes=count((array)$task->likes());
-    $likes=[
-        'likes_number'=>$tasklikes,
-    ];
-
-    return Response::json($likes);
-
-});
-
-Route::put('/tasks/{task_id}',function(Request $request,$task_id){
-    $task = Task::find($task_id);
-
-    $task->task = $request->task;
-    $task->description = $request->description;
-    $task->deadline=$request->deadline;
-
-    $task->save();
-
-    $data=[
-        'id'=>$task->id,
-        'task'=>$task->task,
-        'description'=>$task->description,
-        'created_at'=>$task->created_at->diffForHumans(),
-        'deadline'=>$task->deadline->diffForHumans(),
-    ];
-
-    return Response::json($data);
-});
-
-Route::delete('/tasks/{task_id}',function($task_id){
-    $task = Task::destroy($task_id);
-
-    return Response::json($task);
 });
